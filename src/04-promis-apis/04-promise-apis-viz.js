d3e0f983dccc07d729cbafcef0e357fb96a11722
Tapas Adhikary <tapas.adhikary@gmail.com>
2021-08-18 11:30:23 +0530

    feat: Added cpnditional milliseconds in log and commented on code

--- src/04-promis-apis/04-promise-apis-viz.js
+++ src/04-promis-apis/04-promise-apis-viz.js
@@ -1,29 +1,36 @@
 
-const getTimeClock = () => {
+// Get the time represented in the
+// format of hh:mm:ss(24 hours clock)
+const getTimeClock = (showMs) => {
     const now = new Date();
     let h = now.getHours();
     let m = now.getMinutes();
     let s = now.getSeconds();
+    let ms = now.getMilliseconds();
     m = padWithZero(m);
     s = padWithZero(s);
-    return `${h}:${m}:${s}`; 
+    return showMs ? `${h}:${m}:${s}:${ms}` : `${h}:${m}:${s}`; 
 };
 
+// Pad the single digit numbers with zero
 const padWithZero = (num) => {
     if (num < 10) {num = "0" + num};
     return num;
 }
 
+// Get the delay values for all the colors
 let redDelay = document.getElementById("red-delays-id").value;
 let greenDelay = document.getElementById("green-delays-id").value;;
 let blueDelay = document.getElementById("blue-delays-id").value;;
 console.log(`Initial delays: red - ${redDelay}, green - ${greenDelay}, blue - ${blueDelay}`);
 
+// Get the reject flag for all the colors
 let rejectRed = document.getElementById("red-error-id").checked;
 let rejectGreen = document.getElementById("green-error-id").checked;
 let rejectBlue = document.getElementById("blue-error-id").checked;
 console.log(`Initial rejections: red - ${rejectRed}, green - ${rejectGreen}, blue - ${rejectBlue}`);
 
+// Call this function when a delay is set for a color
 const selectDelay = () => {
     redDelay = document.getElementById("red-delays-id").value;
     greenDelay = document.getElementById("green-delays-id").value;;
@@ -32,6 +39,7 @@ const selectDelay = () => {
     selectValue();
 }
 
+// Call this function when a reject flag is set for a color
 const rejectColor = () => {
     rejectRed = document.getElementById("red-error-id").checked;
     rejectGreen = document.getElementById("green-error-id").checked;
@@ -40,7 +48,7 @@ const rejectColor = () => {
     selectValue();
 }
 
-
+// Call this method when a Promise API is selected
 const selectValue = () => {
     clear();
     const apiSelectBox = document.getElementById("apis-list-id");
@@ -67,6 +75,7 @@ const selectValue = () => {
     }
 };
 
+// Set the value, color, and animation when a Promise Resolves
 const setValues = (color, value, duration) => {
     document.getElementById(`${color}-id`).innerHTML = value;
     document.getElementById(`${color}-id`).parentElement.setAttribute('class', value);
@@ -74,6 +83,7 @@ const setValues = (color, value, duration) => {
     document.getElementById(`${color}-id`).style.animationDuration = duration;
 }
 
+// Clear all the value, color, animations
 const clear = () => {
     const colors = ['red', 'green', 'blue'];
     colors.forEach(color => {
@@ -81,19 +91,22 @@ const clear = () => {
     });
 }
 
+// Clear all the logs from the log board
 const clearLogs = () => {
     const logElem = document.getElementById('log-id');
     logElem.innerHTML = '';
 }
 
+// Add logs to the log board
 const log = (msg, isError) => {
     const logElem = document.getElementById('log-id');
     const logListElem = document.createElement('li');
     isError ? logListElem.setAttribute('class', 'error') : logListElem.removeAttribute('class');
-    logListElem.innerHTML = `<span><b>${getTimeClock()}:</b> ${msg}</span>`;
+    logListElem.innerHTML = `<span><b>${getTimeClock()} -</b> ${msg}</span>`;
     logElem.appendChild(logListElem);
 }
 
+// Handle the Promise.all() API
 const handleAll = async () => {
     try {
         log(`🕛 Started handling all the colors using Promise.all([red, green, blue])`);
@@ -125,6 +138,7 @@ const handleAll = async () => {
     };
 };
 
+// Handle the Promise.any() API
 const handleAny = async () => {
     try {
         log(`🕛 Started handling any of the colors using Promise.any([red, green, blue])`); 
@@ -155,6 +169,7 @@ const handleAny = async () => {
     };
 };
 
+// Handle the Promise.race() API
 const handleRace = async () => {
     try{
         log(`🕛 Started racing of the colors using Promise.race([red, green, blue])`);
@@ -185,6 +200,7 @@ const handleRace = async () => {
     }
 };
 
+// Handle the Promise.allSettled() API
 const handleAllSettled = async () => {
     try {
         log(`🕛 Started settling of the colors using Promise.allSettled([red, green, blue])`); 
@@ -221,6 +237,7 @@ const handleAllSettled = async () => {
     };
 };
 
+// Handle the Promise.resolve() API
 const handleResolve = async () => {
     try {
         log(`🕛 Resolving all colors individually with Promise.resolve(color => red, green, blue)`); 
